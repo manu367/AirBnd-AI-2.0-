73cc2afbffe816337c312b818521dd10de296035
manu pathak <>
2025-06-09 10:34:06 +0530

    automation

--- push-code.bat
+++ push-code.bat
@@ -1,18 +1,8 @@
 @echo off
 :: Check if there are changes
-git status --porcelain > temp_status.txt
-
-:: Count number of changed files
-for /f %%A in ('find /v "" ^< temp_status.txt ^| find /c /v ""') do set count=%%A
-del temp_status.txt
-
-if "%count%"=="0" (
-    echo No changes to commit.
-) else (
-    set /p msg=Enter commit message: 
-    git add .
-    git commit -m "%msg%"
-    git push origin main
-)
-
+git status
+set /p msg=Enter commit message: 
+git add .
+git commit -m "%msg%"
+git push origin main
 pause
